5c7ec9b671085a5fa114d79bd83ee37b5a5943bf
Maciej Katafiasz <mathrick@gmail.com>
2024-03-05 11:50:34 -0800

    Don't wrap tsc parsers. Finish basic treesit-parser-* API

--- treesit-polyfill-core.el
+++ treesit-polyfill-core.el
@@ -32,7 +32,25 @@
 
 (defvar tsp--buffer-parser-map
   (make-hash-table)
-  "Hash table providing a mapping from buffers to their parsers.")
+  "Hash table providing a mapping from buffers to their parsers.
+See also `tsp--parser-buffer-map'.
+
+This is a part of the treesit-polyfill API.")
+
+;; Since parsers are longer-lived and less numerous than nodes, we can
+;; probably afford to keep two mappings (buffers→parsers and parsers→buffers)
+;; and keep them in sync. This allows us to skip wrapping parsers the
+;; way we have to wrap nodes to keep track of their parsers
+(defvar tsp--parser-buffer-map
+  (make-hash-table)
+  "Hash table providing a mapping from parsers to their buffers.
+This table is not meant to be used directly and is NOT guaranteed
+to be up-to-date unless `tsp--sync-parser-buffer-map' has been
+called. Use low-level `tsp-*' or high-leve `treesit-*' functions
+instead of poking this table directly.
+See also `tsp--buffer-parser-map'.
+
+This is a part of the treesit-polyfill API.")
 
 (defun treesit-parser-list (&optional buffer)
   "Return BUFFER’s parser list.
@@ -41,26 +59,6 @@ BUFFER defaults to the current buffer.  If that buffer is an indirect
 buffer, its base buffer is used instead.  That is, indirect buffers
 use their base buffer’s parsers."
   (gethash (tsp--get-target-buffer buffer) tsp--buffer-parser-map))
-
-(defun tsp--wrap-parser (parser buffer)
-  "Return (cons PARSER BUFFER). This is needed because tree-sit.el
-does not provide API to retrieve a parser's buffer, but treesit.el
-does."
-  (cons parser buffer))
-
-(cl-defmacro tsp--unwrap-parser ((parser-var buffer-var) parser &body body)
-  "Helper to unwrap the cons of PARSER BUFFER."
-  (declare (indent defun))
-  `(pcase ,parser
-     ((and `(,,parser-var . ,,buffer-var)
-           (guard (tsc-parser-p ,parser-var)))
-      ,@body)
-     ((pred tsc-parser-p)
-      (let ((,parser-var ,parser)
-            (,buffer-var nil))
-         ,@body))
-     (_
-      (error "%s is not a valid treesit-polyfill parser, should be (cons PARSER BUFFER)" ,parser))))
 
 (defun tsp--get-target-buffer (buffer)
   "Get the target buffer for treesit operations. This means if
@@ -86,7 +84,7 @@ does."
     (_
      (error "%s is not a valid tree-sitter language" lang))))
 
-(cl-defun treesit-parser-create (language &optional buffer no-reuse)
+(defun treesit-parser-create (language &optional buffer no-reuse)
   "Create and return a parser in BUFFER for LANGUAGE.
 
 The parser is automatically added to BUFFER’s parser list, as returned
@@ -100,7 +98,7 @@ That is, indirect buffers use their base buffer’s parsers.  Lisp
 programs should widen as necessary should they want to use a parser in
 an indirect buffer."
   (let* ((buffer (tsp--get-target-buffer buffer))
-         (language (tsp--lang language))
+         (language (tsp--lang-name language))
          (parsers (treesit-parser-list buffer))
          (existing (and (not no-reuse)
                         (first (seq-filter (lambda (parser)
@@ -111,18 +109,40 @@ an indirect buffer."
       (tsc-set-language parser language)
       (setf (gethash buffer tsp--buffer-parser-map)
             (append parsers (list parser))))
-    (tsp--wrap-parser parser buffer)))
+    parser))
+
+(defun treesit-parser-delete (parser)
+  "Delete PARSER from its buffer’s parser list.
+See ‘treesit-parser-list’ for the buffer’s parser list."
+  (let* ((buffer (treesit-parser-buffer parser))
+         (parsers (gethash buffer tsp--buffer-parser-map)))
+    (setf (gethash buffer tsp--buffer-parser-map)
+          (remove parser parsers))
+    ;; FIXME: this might be slow, will need to see in practice
+    (tsp--sync-parser-buffer-map)))
+
+(defun tsp--sync-parser-buffer-map ()
+  "Reflect the contents of `tsp--buffer-parser-map' into `tsp--parser-buffer-map'."
+  (cl-loop for buf being the hash-keys of tsp--buffer-parser-map
+               using (hash-value parsers)
+           ;; cl-loop generates buggy code if we do "for parser in
+           ;; parsers" in the outer loop, have to use nested
+           do (cl-loop for parser in parsers
+                       do (setf (gethash parser tsp--parser-buffer-map) buf))))
 
 (defun treesit-parser-language (parser)
   "Return PARSER’s language symbol.
 This symbol is the one used to create the parser."
-  (tsp--unwrap-parser (parser buffer) parser
-    (tsp--lang-name (tsc-parser-language parser))))
+  (tsp--lang-name (tsc-parser-language parser)))
 
 (defun treesit-parser-buffer (parser)
   "Return the buffer of PARSER."
-  (tsp--unwrap-parser (parser buffer) buffer
-    buffer))
+  (unless (gethash parser tsp--parser-buffer-map)
+    ;; Try syncing the hash tables to see if we can find the buffer
+    (tsp--sync-parser-buffer-map))
+  (or (gethash parser tsp--parser-buffer-map)
+      (error "Parser %s has no known buffer association. It might have been created outside of treesit.el API"
+             parser)))
 
 (defun tsp--wrap-node (node parser)
   "Return (cons NODE PARSER). This is needed because tree-sit.el
