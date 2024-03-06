53d91e1ed45ceb8dc8733bf1d1f494a492a9fee9
Maciej Katafiasz <mathrick@gmail.com>
2024-03-06 14:28:01 -0800

    Finalise treesit-node-* C API implementation
    The only thing missing is checking whether a node is "outdated", which
    needs a lot more bookkeeping to mimic what treesit.c does to tie
    parsers to their buffers and reparse on every change.

--- treesit-polyfill-core.el
+++ treesit-polyfill-core.el
@@ -30,7 +30,7 @@
 (defalias 'treesit-compiled-query-p 'tsc-query-p)
 (defalias 'treesit-query-p 'tsc-query-p)
 
-(defun tsc--sym-name (symbol)
+(defun tsp--sym-name (symbol)
   "Like `symbol-name', but skips initial : in keywords"
   (let ((name (symbol-name symbol)))
     (if (keywordp symbol)
@@ -193,7 +193,7 @@ does."
            (body (if rewrap
                      `((tsp--wrap-node (progn ,@body) ,parser-var))
                    body)))
-      `(defun ,name (,@args)
+      `(cl-defun ,name (,@args)
          ,doc
          (if ,node-var
              (tsp--unwrap-node (,node-var ,parser-var) ,node-var
@@ -202,6 +202,15 @@ does."
               '(error "NODE cannot be nil")))))))
 
 (defmacro tsp--node-alias (symbol args definition &optional doc)
+  "Helper for defining translations from `treesit-node-*' to `tsc-*' functions.
+Useful when there is a 1:1 correspondence. We can't just use
+`defalias' because of the wrapping business we use to keep track
+of nodes' parsers.
+
+SYMBOL is the `treesit-node-*' function to implement, DEFINITION
+is the target `tsc-*' function to use. ARGS is an option list of
+extra arguments (other than NODE, which is implicit) to take and
+pass to the target. DOC is the docstring."
   `(tsp--node-defun ,symbol (node ,@args)
      ,(when doc (list doc))
      (,definition node ,@args)))
@@ -243,8 +252,38 @@ N could be negative, e.g., -1 represents the last child."
          (func (if named #'tsc-get-nth-named-child #'tsc-get-nth-child)))
     (funcall func node n)))
 
-(tsp--node-alias treesit-node-check () FIXME
-  "Return non-nil if NODE has PROPERTY, nil otherwise.")
+(tsp--node-defun treesit-node-check ((node parser) property)
+  "Return non-nil if NODE has PROPERTY, nil otherwise.
+
+PROPERTY could be ‘named’, ‘missing’, ‘extra’, ‘outdated’,
+‘has-error’, or ‘live’.
+
+Named nodes correspond to named rules in the language definition,
+whereas \"anonymous\" nodes correspond to string literals in the
+language definition.
+
+Missing nodes are inserted by the parser in order to recover from
+certain kinds of syntax errors, i.e., should be there but not there.
+
+Extra nodes represent things like comments, which are not required the
+language definition, but can appear anywhere.
+
+A node is \"outdated\" if the parser has reparsed at least once after
+the node was created.
+
+A node \"has error\" if itself is a syntax error or contains any syntax
+errors.
+
+A node is \"live\" if its parser is not deleted and its buffer is
+live."
+  (pcase property
+    ('named (tsc-node-named-p node))
+    ('missing (tsc-node-missing-p node))
+    ('extra (tsc-node-extra-p node))
+    ('has-error (tsc-node-has-error-p node))
+    ;; `nil' is a valid input to `buffer-live-p', so this is safe to do
+    ('live (buffer-live-p (treesit-parser-buffer parser)))
+    ('outdated (error "`outdated' property is not currently implemented"))))
 
 (tsp--node-defun treesit-node-field-name-for-child (node n)
   "Return the field name of the Nth child of NODE.
@@ -261,49 +300,72 @@ N could be negative, e.g., -1 represents the last child."
          (cursor (tsc-make-cursor node)))
     (tsc-goto-first-child cursor)
     (cl-loop for i from 1 to n do (tsc-goto-next-sibling cursor))
-    (tsc--sym-name (tsc-current-field cursor))))
+    (tsp--sym-name (tsc-current-field cursor))))
 
 (tsp--node-defun treesit-node-child-count (node &optional named)
   "Return the number of children of NODE.
 
 If NAMED is non-nil, count named children only.  NAMED defaults to
 nil.  If NODE is nil, return nil."
-  (if named (tsc-count-named-children node)
+  (if named
+      (tsc-count-named-children node)
     (tsc-count-children node)))
 
-(tsp--node-alias treesit-node-child-by-field-name () FIXME
+(tsp--node-defun treesit-node-child-by-field-name ((node _ :rewrap t) field-name)
   "Return the child of NODE with FIELD-NAME.
-Return nil if there is no such child.  If NODE is nil, return nil.")
+Return nil if there is no such child.  If NODE is nil, return nil."
+  (tsc--get-child-by-field-name node field-name))
 
-(tsp--node-alias treesit-node-next-sibling () FIXME
-    "Return the next sibling of NODE.
+(tsp--node-defun treesit-node-next-sibling (node &optional named)
+  "Return the next sibling of NODE.
 
 Return nil if there is no next sibling.  If NAMED is non-nil, look for named
-siblings only.  NAMED defaults to nil.  If NODE is nil, return nil.")
+siblings only. NAMED defaults to nil.  If NODE is nil, return nil."
+  (if named
+      (tsc-get-next-named-sibling node)
+    (tsc-get-next-sibling node)))
 
-(tsp--node-alias treesit-node-prev-sibling () FIXME
+(tsp--node-defun treesit-node-prev-sibling (node &optional named)
   "Return the previous sibling of NODE.
 
 Return nil if there is no previous sibling.  If NAMED is non-nil, look
 for named siblings only.  NAMED defaults to nil.  If NODE is nil,
-return nil.")
+return nil."
+  (if named
+      (tsc-get-prev-named-sibling node)
+    (tsc-get-prev-sibling node)))
 
-(tsp--node-alias treesit-node-first-child-for-pos () FIXME
+;; FIXME: Currently this disagrees with treesit.el on the last position.
+;; Ie. if parssing a python string with 68 positions, our implementation
+;; returns a block spanning (1 . 68) for pos = 68, but treesit.el
+;; returns nil. I don't know who's right.
+(tsp--node-defun treesit-node-first-child-for-pos ((node _ :rewrap t) pos &optional named)
   "Return the first child of NODE for buffer position POS.
 
 Specifically, return the first child that extends beyond POS.
 Return nil if there is no such child.
 If NAMED is non-nil, look for named children only.  NAMED defaults to nil.
 Note that this function returns an immediate child, not the smallest
-(grand)child.  If NODE is nil, return nil.")
+(grand)child.  If NODE is nil, return nil."
+  (let* ((cursor (tsc-make-cursor node))
+         (candidate (progn
+                      (tsc-goto-first-child-for-position cursor pos)
+                      (tsc-current-node cursor))))
+    (if (or (not named)
+            (tsc-node-named-p candidate))
+        candidate
+      (tsc-get-next-named-sibling candidate))))
 
-(tsp--node-alias treesit-node-descendant-for-range () FIXME
-    "Return the smallest node that covers buffer positions BEG to END.
+(tsp--node-defun treesit-node-descendant-for-range (node beg end &optional named)
+  "Return the smallest node that covers buffer positions BEG to END.
 
 The returned node is a descendant of NODE.
 Return nil if there is no such node.
 If NAMED is non-nil, look for named child only.  NAMED defaults to nil.
-If NODE is nil, return nil.")
+If NODE is nil, return nil."
+  (if named
+      (tsc-get-named-descendant-for-position-range node)
+    (tsc-get-descendant-for-position-range node)))
 
 (tsp--node-alias treesit-node-eq () tsc-node-eq
   "Return non-nil if NODE1 and NODE2 refer to the same node.
